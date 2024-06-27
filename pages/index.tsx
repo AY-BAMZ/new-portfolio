201221196934741800317958c07e9ef8c50da3f7
ayobami paul <aybamz181@gmail.com>
2024-06-27 21:37:21 +0100

    new changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,20 @@ import ParticlesJs from "@/components/Particles";
 import SideBar from "@/widgets/SideBar";
 
 export default function HomePage() {
-
   return (
     <div className="relative">
-    <ParticlesJs />
-    <div className="absolute w-screen h-screen flex ">
-      <span className="h-full max-h-full">
-        <SideBar />
-      </span>
-      <h1>Hello</h1>
-      <p className="text-white text-[30px]">Hello</p>
-    </div>
-
+      <ParticlesJs />
+      <div className="absolute w-screen h-screen flex ">
+        <span className="h-full max-h-full">
+          <SideBar />
+        </span>
+        <span className="flex-grow w-full flex justify-center items-center">
+          <span className="flex flex-col gap-4 items-center">
+            <h1 className="text-[52px]">Ayobami Paul Ade.</h1>
+            <p className="text-white text-[30px]">I'm a Software Engineer</p>
+          </span>
+        </span>
+      </div>
     </div>
   );
 }
--- widgets/SideBar/index.tsx
+++ widgets/SideBar/index.tsx
@@ -1,66 +1,150 @@
-import { cn } from '@/lib'
-import { getTheme } from '@/lib/localStorage'
-import { HomeHashtag } from 'iconsax-react'
-import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
+import { cn } from "@/lib";
+import { getTheme } from "@/lib/localStorage";
+import {
+  Book1,
+  BrifecaseTick,
+  Firstline,
+  Grid3,
+  HomeHashtag,
+  UserOctagon,
+} from "iconsax-react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 function SideBar() {
-    const links = [
-        {
-            link: "/",
-            Icon: HomeHashtag,
-            text: "Home"
-        },
-        {
-            link: "/",
-            Icon: HomeHashtag,
-            text: "Home"
-        },
-        {
-            link: "/",
-            Icon: HomeHashtag,
-            text: "Home"
-        },
-        {
-            link: "/",
-            Icon: HomeHashtag,
-            text: "Home"
-        },
-        {
-            link: "/",
-            Icon: HomeHashtag,
-            text: "Home"
-        },
-    ]
-    const theme = getTheme()
-    const [active, setActive] = useState(links[0])
+  const router = useRouter();
+  const pathname = router.pathname;
+  const links = [
+    {
+      link: "/",
+      Icon: HomeHashtag,
+      text: "Home",
+    },
+    {
+      link: "/about",
+      Icon: UserOctagon,
+      text: "About",
+    },
+    {
+      link: "/skills",
+      Icon: Book1,
+      text: "Skills",
+    },
+    {
+      link: "/experience",
+      Icon: BrifecaseTick,
+      text: "Experience",
+    },
+    {
+      link: "/portfolio",
+      Icon: Grid3,
+      text: "Portfolio",
+    },
+    {
+      link: "/contacts",
+      Icon: Firstline,
+      text: "Contacts",
+    },
+  ];
+  console.log("pathname :>> ", pathname);
+  const theme = getTheme();
+  const [active, setActive] = useState(links[0]?.text);
 
-    const [color, setColor] = useState("red")
+  const [color, setColor] = useState("orange");
 
-    useEffect(() => {
-      if(theme){
-        setColor(theme)
-      } else {
-        setColor("red")
-      }
-    }, [theme])
-    
+  useEffect(() => {
+    if (theme) {
+      setColor(theme);
+    } else {
+      setColor("orange");
+    }
+    if (pathname) {
+      setActive(pathname);
+    }
+  }, [theme]);
+
+  const handleSelect = (value: any) => {
+    setActive(value?.link);
+    router.push(`${value?.link}`);
+  };
   return (
-    <span className='flex flex-col h-[100vh] w-[160px] bg-[#00000025]'>
-        <span className='w-full flex items-center justify-center aspect-square border-b-[1px] border-b-black-500'>
-            <Image 
-              src="https://res.cloudinary.com/dmixz7eur/image/upload/v1673583454/my%20portfolio/logo_wzrddg.png"
-              alt=""
-              width={70}
-              height={70}/>
-        </span>
-        <span>
-            {links.map((item: any, index: any)=>(<span key={index} className={cn(`flex w-full aspect-[6/3] items-center justify-center cursor-pointer`, {"hover:text-primary-blue": color === "blue"})}>
-<item.Icon size={48} variant="Bold" className={cn({"hover:text-primary-blue": color === "blue", "hover:text-primary-red": color === "red", })}/>
-            </span>))}
-        </span>
+    <span className="flex flex-col h-[100vh] w-[150px] bg-[#00000035]">
+      <span className="w-full flex items-center justify-center aspect-square border-b-[1px] border-b-black-500">
+        <Image
+          src="https://res.cloudinary.com/dmixz7eur/image/upload/v1673583454/my%20portfolio/logo_wzrddg.png"
+          alt=""
+          width={64}
+          height={64}
+        />
+      </span>
+      <span className="py-7">
+        {links.map((item: any, index: any) => (
+          <span
+            key={index}
+            className={cn(
+              `flex flex-col gap-1 w-full aspect-[9/5] items-center justify-center cursor-pointer`,
+              {
+                "hover:text-primary-blue": color === "blue",
+                "hover:text-primary-red": color === "red",
+                "hover:text-primary-green": color === "green",
+                "hover:text-primary-yellow": color === "yellow",
+                "hover:text-primary-cray": color === "cray",
+                "hover:text-primary-purple": color === "purple",
+                "hover:text-primary-orange": color === "orange",
+              },
+              {
+                "text-primary-blue": color === "blue" && active === item?.link,
+                "text-primary-red": color === "red" && active === item?.link,
+                "text-primary-green":
+                  color === "green" && active === item?.link,
+                "text-primary-yellow":
+                  color === "yellow" && active === item?.link,
+                "text-primary-cray": color === "cray" && active === item?.link,
+                "text-primary-purple":
+                  color === "purple" && active === item?.link,
+                "text-primary-orange":
+                  color === "orange" && active === item?.link,
+              }
+            )}
+            onClick={() => handleSelect(item)}
+          >
+            <item.Icon
+              size={40}
+              variant="Bold"
+              className={cn(
+                {
+                  "hover:text-primary-blue": color === "blue",
+                  "hover:text-primary-red": color === "red",
+                  "hover:text-primary-green": color === "green",
+                  "hover:text-primary-yellow": color === "yellow",
+                  "hover:text-primary-cray": color === "cray",
+                  "hover:text-primary-purple": color === "purple",
+                  "hover:text-primary-orange": color === "orange",
+                },
+                {
+                  "text-primary-blue":
+                    color === "blue" && active === item?.text,
+                  "text-primary-red": color === "red" && active === item?.text,
+                  "text-primary-green":
+                    color === "green" && active === item?.text,
+                  "text-primary-yellow":
+                    color === "yellow" && active === item?.text,
+                  "text-primary-cray":
+                    color === "cray" && active === item?.text,
+                  "text-primary-purple":
+                    color === "purple" && active === item?.text,
+                  "text-primary-orange":
+                    color === "orange" && active === item?.text,
+                }
+              )}
+            />
+            <p className="text-[10px] font-light">{item?.text}</p>
+          </span>
+        ))}
+      </span>
     </span>
-  )
+  );
 }
 
-export default SideBar
+export default SideBar;

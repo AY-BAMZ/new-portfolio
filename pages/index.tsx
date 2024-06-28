40c4b14902de209ac66555d09170ba6881ddd3bd
ayobami paul <aybamz181@gmail.com>
2024-06-28 08:31:46 +0100

    new changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <Wrapper>
       <span className="flex flex-col gap-4 items-center">
         <h1 className="text-[52px]">Ayobami Paul Ade.</h1>
-        <p className="text-white text-[30px]">I'm a Software Engineer</p>
+        <p className="text-white text-[30px]">Software Engineer</p>
       </span>
     </Wrapper>
   );

c166f0384f12a879b7b63f29d6859a0b2e00c024
ayobami paul <aybamz181@gmail.com>
2024-06-28 14:03:54 +0100

    new changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,12 @@
-import '@mantine/core/styles.css';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
-import { theme } from '../theme';
-import "@/styles/globals.css"
+import "@mantine/core/styles.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider>
       <Head>
         <title>Ayobami paul</title>
         <meta
